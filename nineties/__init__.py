582b651c0c7e4ec5cb57a679d4ab1af16fbe8c3d
Stefan Hagen <stefan@hagen.link>
2022-12-29 19:01:36 +0100

    synced version

--- nineties/__init__.py
+++ nineties/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2022.7.24+parent.a35bd82d'
+# [[[end]]] (checksum: 4b24cff310f022c94c50d2979b85c7d5)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
